53e44c881f4f1252df400fed9a465dfafe818385
Himerall <maxfantomr@yandex.ru>
2026-01-01 01:34:45 +0300

    Many changes

--- k.cpp
+++ k.cpp
@@ -1,14 +1,14 @@
+#include <cstdint>
+#include <iomanip>
 #include <iostream>
 #include <ctime>
 #include "cpptotp/src/libcppotp/bytes.h"
 #include "cpptotp/src/libcppotp/otp.h"
 
-uint32_t generateTotp(const CppTotp::Bytes::ByteString &key,
-                      uint64_t unixTime,
-                      size_t digits = 6) {
+uint32_t generateTotp(const CppTotp::Bytes::ByteString &key, uint64_t unixTime, size_t digits = 6) {
     using namespace CppTotp;
     uint64_t start = 0;
-    uint64_t step  = 30;
+    uint64_t step  = 10;
     return totp(key, unixTime, start, step, digits, hmacSha1_64);
 }
 
@@ -20,11 +20,19 @@ int main() {
         reinterpret_cast<const uint8_t*>(secr) + 20
     );
     
-    
+    std::cout << Bytes::toBase32(key);
+    if (Bytes::fromBase32(Bytes::toBase32(key)) == key) std::cout << "-1\n";
+    std::time_t now = std::time(nullptr);
+    uint32_t code = 1;
+    uint32_t last_code = 0;
+
     while (true) {
-        std::time_t now = std::time(nullptr);
-        uint32_t code = generateTotp(key, now);
-        std::cout << code << "\n";  // Вывод 6-значного TOTP-кода
+        now = std::time(nullptr);
+        code = generateTotp(key, now);
+        // std::cout << code << "\n";
+        std::cout.fill('0');
+        std::cout.width(6); 
+        if (last_code!=code) std::cout << code  << "\n"; last_code = code;
     }
     return 0;
 }
